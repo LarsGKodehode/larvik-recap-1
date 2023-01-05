705fa8d64c83f811196957b010b573590df379cb
LarsG <lars.ga.solheim@gmail.com>
2023-01-05 11:31:51 +0100

    finished cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
+// Libraries
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Components
 import App from './App'
-import './index.css'
+
+// CSS
+import './styles/basic.css'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
